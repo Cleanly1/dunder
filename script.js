8502ba57d83d7c3887a99622b13dd1aa1f238e4e
Cleanly <oskar.turesson99@gmail.com>
2019-11-10 21:40:12 +0100

    added more styling to recommenadtions

--- script.js
+++ script.js
@@ -33,6 +33,28 @@ const buttonClick1 = function(){
   window.document.getElementsByClassName('orderBox')[0].style.background = '#FFFFFF';
 }
 
+const displayScroll = function() {
+  if (scrollY > 200) {
+    window.document.getElementsByClassName('scrollTop')[0].style = 'display:flex;'
+  }else {
+    window.document.getElementsByClassName('scrollTop')[0].style = 'display:none;'
+  }
+}
+
+const scrollToTop = function() {
+
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+
+window.onscroll = displayScroll;
+
+window.document.getElementsByClassName('scrollTop')[0].addEventListener("click", scrollToTop);
 
 window.document.getElementsByClassName('orderBox')[0].addEventListener("click", buttonClick0);
 
